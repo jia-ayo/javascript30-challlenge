bede772d5c5043681540b5978bd8fa727a841888
jia-ayo <milkjia11@gmail.com>
2022-11-30 12:56:29 +0100

    middle day 19

--- webcam_fun/scripts.js
+++ webcam_fun/scripts.js
@@ -5,10 +5,21 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');  
 function getVideo(){
     navigator.mediaDevices.getUserMedia({ video: true, audio: false})
-      .then(localMediaStream =>{
-        console.log(localMediaStream)
-        video.src = window.URL.createObjectURL(localMediaStream);
+      .then(MediaStream =>{
+        console.log(MediaStream)
+        // video.src = window.URL.createObjectURL(MediaStream);
+        video.srcObject=MediaStream;
         video.play(); 
-     });
+     })
+     .catch(err =>{
+       console.log('OH NO!!', err);
+     })
+}
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
 }
 getVideo()
